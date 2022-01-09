b905aa96e35ecda1b885d7599f644d72a73a24c3
Javier Pérez <javier.perez@zippedi.com>
2022-01-09 17:18:16 -0300

    HOTFIX: error de numeracion en ingresos uci

--- API/main.py
+++ API/main.py
@@ -103,13 +103,13 @@ def vacunas():
         for i in range(len(df)):
             datos={}
             datos["semana"]=df_list[i][0]
-            datos["sin_vacunar"]=df_list[i][7]
-            datos["1_dosis"]=df_list[i][8]
-            datos["2_dosis"]=df_list[i][9]
-            datos["completo"]=df_list[i][10]
-            datos["dosis_unica"]=df_list[i][11]
-            datos["dosis_unica_completo"]=df_list[i][12]
-            datos["refuerzo"]=df_list[i][13]
+            datos["sin_vacunar"]=df_list[i][8]
+            datos["1_dosis"]=df_list[i][9]
+            datos["2_dosis"]=df_list[i][10]
+            datos["completo"]=df_list[i][11]
+            datos["dosis_unica"]=df_list[i][12]
+            datos["dosis_unica_completo"]=df_list[i][13]
+            datos["refuerzo"]=df_list[i][14]
             casos.append(datos)
         return{
             "casos":casos
